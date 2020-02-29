e17b4c22c1bb4435af170f4fd32e7e533912646b
charliegabriella <charlotkroon@gmail.com>
2020-02-29 17:11:03 +0100

    Create Movies

--- sequelize-rest.js
+++ sequelize-rest.js
@@ -1,7 +1,10 @@
 const Sequelize = require("sequelize"); //initialize the database connection with Sequelize.
-const db = require("../db"); //initialize the database connection with Sequelize.
 const express = require("express");
 const bodyparser = require("body-parser");
+
+const databaseUrl = "postgres://postgres:secret@localhost:5432/postgres";
+const db = new Sequelize(databaseUrl);
+
 const Movie = db.define("movie", {
   title: {
     type: Sequelize.STRING
@@ -14,9 +17,33 @@ const Movie = db.define("movie", {
   }
 });
 
-//6. use the model create() method to insert 3 rows of example data.
-//This logic should happen after the model synchronization completes.
-//The data should persist. Restarting the API should not cause any data to be lost.
+db.sync();
+
+sequelize
+  .sync() // Create tables if necessary
+  .then(() =>
+    Promise.all([
+      // //Use the model create() method to insert 3 rows of example data.
+      Movie.create({
+        title: "Harry Potter and the blablabla Stone",
+        yearOfRelease: "2001",
+        synopis:
+          "11 year old Harry thought he was normal but then a giant comes to his house and tells him that he's a wizard. Harry asks: 'I am what?'. The giant says: 'a wizard'. And Harry believes him."
+      }),
+      Movie.create({
+        title: "Seinfeld",
+        yearOfRelease: "1989",
+        synopsis:
+          "this is not a movie but a tvshow, but if you watch a lot of episodes back to back to back it feels like movie, right?"
+      }),
+      Movie.create({
+        title: "call me by your name",
+        yearOfRelease: "2017",
+        synopsis:
+          "a movie that makes you wish you had the exact same summer house in italy like the people in the movie and makes you sign up for duolingo to learn french and italian, so you can speak one part of a sentence in french and the other part of the sentence in italian like elio does"
+      })
+    ])
+  );
 
 //7.
 //create a new movie resource
