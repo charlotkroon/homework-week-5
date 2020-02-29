ec96a7ca2d8f7c79ad8f8bf4f0cb7f802f705630
charliegabriella <charlotkroon@gmail.com>
2020-02-29 12:05:41 +0100

    Added API limit exceeded

--- messages-api.js
+++ messages-api.js
@@ -7,9 +7,21 @@ const port = 3000;
 app.get("/", (req, res) => res.send("Hello World!"));
 app.use(bodyparser.json());
 
+// 6. The API should only log the message five times.
+let amountOfRequests = 0;
+const limit = (req, res, next) => {
+  if (count > 5) {
+    //More than 6 requests?...
+    res.status(429).end(); //...send 'too many requests' status code
+  } else {
+    next();
+  }
+};
+
 //POST requests to the /messages URI
 app.post("/messages", (req, res) => {
   if (!req.body.text) {
+    amountOfRequests++; //increment amount of requests
     res.status(400).end();
   } else {
     res.send({ message: "This is the message that was sent" });
@@ -17,11 +29,3 @@ app.post("/messages", (req, res) => {
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-
-// 6. The API should only log the message five times.
-// After receiving five messages, sixth request should be sent a response that indicates the HTTP status
-// for "Too Many Requests". Make sure the correct HTTP status code is sent
-// (Google it if you haven't seen this status message before).
-// Although there are libraries to implement such limits, do not use them! Implement the logic yourself.
-
-//7.Put the message limit logic from the previous step into a middleware function. It should behave the same.
--- sequelize-rest.js
+++ sequelize-rest.js
@@ -14,4 +14,16 @@ const Movie = db.define("movie", {
   }
 });
 
+//6. use the model create() method to insert 3 rows of example data.
+//This logic should happen after the model synchronization completes.
+//The data should persist. Restarting the API should not cause any data to be lost.
+
+//7.
+//create a new movie resource
+// read all movies (the collections resource)
+// read a single movie resource
+// update a single movie resource
+// delete a single movie resource You don't need any special logic. A standard REST implementation is ok.
+
+//8. Make sure that your handlers send back 404 status codes when appropriate.
 module.exports = Movie;
